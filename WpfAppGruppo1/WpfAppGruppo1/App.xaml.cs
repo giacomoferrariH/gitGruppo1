13631434a2728bfff4ccc265fc0a2b05f094a9c1
Giacomo Ferrari <GIACOMO.FERRARI@local.ittcesena.it>
2024-10-07 09:01:14 +0200

    Release 0.0.0: Creazione Soluzione VisualStudio

---
+++ WpfAppGruppo1/WpfAppGruppo1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppGruppo1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
