46cdb08cfcd72a98a79805118985a6fbd1740d43
Giacomo Ferrari <GIACOMO.FERRARI@local.ittcesena.it>
2024-10-07 09:46:37 +0200

    Release 0.0.1: Modifca Main - Ferrari

--- WpfAppGruppo1/WpfAppGruppo1/MainWindow.xaml.cs
+++ WpfAppGruppo1/WpfAppGruppo1/MainWindow.xaml.cs
@@ -20,5 +20,41 @@ namespace WpfAppGruppo1
         {
             InitializeComponent();
         }
+        public void setTitolo(string titolo)
+        {
+
+        }
+        public void setAutore(string autore)
+        {
+
+        }
+        public void setDurata(int durata)
+        {
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // {Application.Current.MainWindow.Title} <-- titolo del MAinWindow
+            MessageBox.Show("");
+            Console.WriteLine("Scrivi il titolo"); // Cambiare il Console
+            string titolo = Console.ReadLine(); // ''
+            setTitolo(titolo);
+
+            Console.WriteLine("Scrivi l'autore del brano"); // ''
+            string autore = Console.ReadLine(); //''
+            setAutore(autore);
+
+            Console.WriteLine("Scrivi la durata"); //''
+            int durata = int.Parse(Console.ReadLine()); //''
+            setDurata(durata);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string autore = "";
+            string titolo = "";
+            int durata = -1;
+            MessageBox.Show($"Autore: {autore}; Titolo = {titolo}; durata = {durata}");
+        }
     }
 }
