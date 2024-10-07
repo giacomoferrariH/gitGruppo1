73d2cc4bfa3df19b1a330de46e7c1356d4f0a21e
Mattia Fabbri (4H) <MATTIA.FABBRI.4h@local.ittcesena.it>
2024-10-07 09:46:52 +0200

    Release CD 0.1.0 creazione classe CD

---
+++ WpfAppGruppo1/WpfAppGruppo1/CD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppGruppo1
+{
+
+    internal class CD
+    {
+        
+        string _titolo;
+        string _autore;
+        List<string> _brani; 
+
+        public CD (string titolo, string autore, List<string> brani)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _brani = new List<string>();
+            for (int i = 0; i < brani.Count; i++)
+            {
+                _brani[i] = brani[i];
+            }
+        }
+
+        private string getTitolo() { return _titolo;}
+
+        private string getAutore() { return _autore; }
+
+        private void setTitolo(string newTitolo)  {_titolo = newTitolo;}
+        private void setAutore(string newAutore)  {_autore = newAutore;}
+
+
+
+    }
+}
