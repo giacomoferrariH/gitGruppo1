be4918d9b515af6bea9a8371d8b2eaf829539f3c
Giacomo Ferrari <GIACOMO.FERRARI@local.ittcesena.it>
2024-10-14 09:05:51 +0200

    Release 0.0.2

--- WpfAppGruppo1/WpfAppGruppo1/MainWindow.xaml.cs
+++ WpfAppGruppo1/WpfAppGruppo1/MainWindow.xaml.cs
@@ -1,60 +1,109 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfAppGruppo1
+namespace MusicLibraryApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class __Brano
+    {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int Durata { get; set; }
+
+        public __Brano(string titolo, string autore, int durata)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+        public bool shortSong(int durata)
+        {
+            return false;
+        }
+    }
+    public class _CD
+    {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int Durata { get; set; }
+        public List<__Brano> Brani { get; set; }
+        public _CD(string titolo, string autore, List<__Brano> brani)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
     public partial class MainWindow : Window
     {
+        private List<__Brano> brani;
+        private _CD cd;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-        public void setTitolo(string titolo)
-        {
-
-        }
-        public void setAutore(string autore)
-        {
-
-        }
-        public void setDurata(int durata)
-        {
-
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // {Application.Current.MainWindow.Title} <-- titolo del MAinWindow
-            MessageBox.Show("");
-            Console.WriteLine("Scrivi il titolo"); // Cambiare il Console
-            string titolo = Console.ReadLine(); // ''
-            setTitolo(titolo);
-
-            Console.WriteLine("Scrivi l'autore del brano"); // ''
-            string autore = Console.ReadLine(); //''
-            setAutore(autore);
-
-            Console.WriteLine("Scrivi la durata"); //''
-            int durata = int.Parse(Console.ReadLine()); //''
-            setDurata(durata);
+            brani = new List<__Brano>();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void BtnAggiungiBrano_Click(object sender, RoutedEventArgs e)
         {
-            string autore = "";
-            string titolo = "";
-            int durata = -1;
-            MessageBox.Show($"Autore: {autore}; Titolo = {titolo}; durata = {durata}");
+            string titolo = txtTitoloBrano.Text;
+            string autore = txtAutoreBrano.Text;
+            int durata;
+
+            if (int.TryParse(txtDurataBrano.Text, out durata))
+            {
+                __Brano nuovoBrano = new __Brano(titolo, autore, durata);
+                brani.Add(nuovoBrano);
+                lstBrani.Items.Add(nuovoBrano.ToString());
+                txtTitoloBrano.Clear();
+                txtAutoreBrano.Clear();
+                txtDurataBrano.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Inserisci una durata valida.");
+            }
+        }
+
+        private void BtnCreaCD_Click(object sender, RoutedEventArgs e)
+        {
+            if (brani.Count > 0)
+            {
+                string titoloCD = txtTitoloCD.Text;
+                string autoreCD = txtAutoreCD.Text;
+
+                cd = new _CD(titoloCD, autoreCD, brani);
+                MessageBox.Show("CD creato con successo!");
+                txtTitoloCD.Clear();
+                txtAutoreCD.Clear();
+                txtBraniCD.Items.Clear();
+                foreach (var brano in brani)
+                {
+                    txtBraniCD.Items.Add(brano.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Aggiungi almeno un brano prima di creare il CD.");
+            }
+        }
+
+        private void BtnMostraDurataCD_Click(object sender, RoutedEventArgs e)
+        {
+            if (cd != null)
+            {
+                MessageBox.Show($"Durata totale del CD: {cd.Durata} secondi");
+            }
+            else
+            {
+                MessageBox.Show("Nessun CD creato.");
+            }
         }
     }
 }
