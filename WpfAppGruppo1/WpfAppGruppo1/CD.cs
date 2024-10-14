17db61812ff696877bf28563795d2e1e29f45d5d
Mattia Fabbri (4H) <MATTIA.FABBRI.4h@local.ittcesena.it>
2024-10-14 09:07:59 +0200

    Release 1.0.0 Classe CD finita

--- WpfAppGruppo1/WpfAppGruppo1/CD.cs
+++ WpfAppGruppo1/WpfAppGruppo1/CD.cs
@@ -1,36 +1,76 @@
-﻿using System;
+﻿//Mattia Fabbri 4H 07/10/2024
+
+using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppGruppo1
 {
+    class _Brano
+    {
+        public int Durata { get; set; }
+
+        public string Titolo { get; set; }
+    }
+
+
 
     internal class CD
     {
-        
         string _titolo;
         string _autore;
-        List<string> _brani; 
+        List<_Brano> _brani; //modificare string con brani
 
-        public CD (string titolo, string autore, List<string> brani)
+        public CD (string titolo, string autore, List<_Brano> brani)
         {
             _titolo = titolo;
             _autore = autore;
-            _brani = new List<string>();
+            _brani = new List<_Brano>();
+            // realloc brani della lista
             for (int i = 0; i < brani.Count; i++)
             {
                 _brani[i] = brani[i];
             }
         }
 
-        private string getTitolo() { return _titolo;}
+        public string Titolo
+        {
+            get { return _titolo; }
+            set {_titolo = value; }
+        }
 
-        private string getAutore() { return _autore; }
+        public string Autore
+        {
+            get { return _autore; }
+            set { _autore = value; }
+        }
 
-        private void setTitolo(string newTitolo)  {_titolo = newTitolo;}
-        private void setAutore(string newAutore)  {_autore = newAutore;}
+        public override string ToString()
+        {
+            // mi ritorna una stringa con tutti i titoli delle canzoni
+            string allTitle = "";
+            for(int i = 0; i < _brani.Count; i++)
+            {
+                allTitle += $"{_brani[i].Titolo} "; //usare metodo toString() della classe brano
+            }
+            return allTitle; 
+        }
+
+        public int Durata 
+        {  // ritorna duarata totale del CD
+            get 
+            { 
+                int durata = 0;
+                for (int i = 0; i < _brani.Count; i++)
+                {
+                    durata += _brani[i].Durata;
+                }
+                return durata;
+            }
+        }
 
 
 
